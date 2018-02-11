db5f0d9ab8db41d029310f6b63b2a5d6828bcc06
Tao Yao <yaotao.nie@gmail.com>
2018-02-11 17:17:42 -0600

    Changed the VALI_RATIO to TEST_RATIO.

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import os
 flags = tf.app.flags
 flags.DEFINE_string('dir_prefix', 'E:\\Dropbox\\Code\\FMNIST-Git\\data\\', 'directory where MNIST is located')
 flags.DEFINE_string('save_dir', '.\\output', 'directory where model graph and weights are saved')
-flags.DEFINE_float('test_train_ratio', 0.95, 'train/raw_data ratio')
-flags.DEFINE_float('vali_train_ratio', 0.9, 'vali/org_train ratio')
+flags.DEFINE_float('test_train_ratio', 0.85, 'train/raw_data ratio')
+flags.DEFINE_float('vali_train_ratio', 0.85, 'vali/org_train ratio')
 flags.DEFINE_float('REG_COEFF', 0.001, 'regularization coefficient')
 flags.DEFINE_integer('batch_size', 32, '')
 flags.DEFINE_integer('max_epoch_num', 100, '')
@@ -32,8 +32,8 @@ train_images, train_labels, vali_images, vali_labels = \
 images = labels = None
 
 if __name__ == '__main__':
-    imgplot = plt.imshow(test_images[0].reshape((28, 28)))
-    plt.show()
+    # imgplot = plt.imshow(test_images[0].reshape((28, 28)))
+    # plt.show()
 
     x = tf.placeholder(tf.float32, [None, 784], name='input_placeholder')
     x_normalized = x / 255.0
@@ -89,46 +89,43 @@ if __name__ == '__main__':
             avg_test_ce = sum(ce_vals) / len(ce_vals)
             print('TEST CROSS ENTROPY: ' + str(avg_test_ce))
             print('TEST CONFUSION MATRIX:')
-            print(str(sum(conf_mxs)))
+            conf_mxs = sum(conf_mxs)
+            print(str(conf_mxs))
+            count_correct = 0
+            count_sum = sum(sum(conf_mxs))
+            for i in range(conf_mxs.shape[0]):
+                count_correct += conf_mxs[i][i]
+            test_ratio = count_correct * 1.0 / count_sum
+            print("TEST CORRECT RATIO: ", test_ratio)
 
             # report validation loss
             ce_vals = []
-            conf_mxs = []
             for i in range(vali_num_examples // batch_size):
-                batch_xs = vali_images[i * batch_size:(i + 1) * batch_size, :]
-                batch_ys = vali_labels[i * batch_size:(i + 1) * batch_size, :]
-                validation_ce, vali_conf_matrix = session.run([mean_total_loss, confusion_matrix_op],
-                                                              {x: batch_xs, y: batch_ys})
+                batch_xs = test_images[i * batch_size:(i + 1) * batch_size, :]
+                batch_ys = test_labels[i * batch_size:(i + 1) * batch_size, :]
+                validation_ce = session.run(mean_total_loss, {x: batch_xs, y: batch_ys})
                 ce_vals.append(validation_ce)
-                conf_mxs.append(vali_conf_matrix)
-            conf_mxs = sum(conf_mxs)
-            count_wrong = 0
-            count_sum = 0
-            for i in range(conf_mxs.shape[0]):
-                for j in range(conf_mxs.shape[1]):
-                    count_sum += conf_mxs[i][j]
-                    if i != j:
-                        count_wrong += conf_mxs[i][j]
-            vali_ratio = 1 - count_wrong * 1.0 / count_sum
-
-            print("VALIDATION CORRECT RATIO: ", vali_ratio)
             avg_test_ce = sum(ce_vals) / len(ce_vals)
             validation_val.append(avg_test_ce)
             print('VALIDATION CROSS ENTROPY: ' + str(avg_test_ce))
+
+            # judge early stop
             if len(validation_val) >= 2 and validation_val[-1] > validation_val[-2]:
                 validation_count = validation_count + 1
                 print("EARLY! ", validation_count)
             else:
                 validation_count = 0
+
             if validation_count >= FLAGS.patience:
                 early_pos.append(epoch)
                 print('Early Stop!!')
                 break
-            if vali_ratio>0.88:
+            if test_ratio > 0.88:
                 file_name = "homework_1-0_{ep}_{rate}_{rate2}".format(ep=epoch,
-                                                                      rate=int(vali_ratio * 100),
-                                                                      rate2=int(vali_ratio * 10000 % 100))
-                path_prefix = saver.save(session, os.path.join(FLAGS.save_dir, file_name), global_step=global_step_tensor)
+                                                                      rate=int(test_ratio * 100),
+                                                                      rate2=int(test_ratio * 10000 % 100))
+                path_prefix = saver.save(session, os.path.join(FLAGS.save_dir, file_name),
+                                         global_step=global_step_tensor)
                 print(path_prefix)
 
         x = range(len(validation_val))
